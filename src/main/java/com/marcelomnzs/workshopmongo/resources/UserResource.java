4375ab23a6c264c384e3676e4c8c54c8501daafe
Marcelo Antonio <marceloa.mnzs@gmail.com>
2025-10-03 11:55:28 -0300

    Conectando ao MongoDB com repository e service

--- src/main/java/com/marcelomnzs/workshopmongo/resources/UserResource.java
+++ src/main/java/com/marcelomnzs/workshopmongo/resources/UserResource.java
@@ -1,6 +1,8 @@
 package com.marcelomnzs.workshopmongo.resources;
 
 import com.marcelomnzs.workshopmongo.domain.User;
+import com.marcelomnzs.workshopmongo.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,15 +15,12 @@ import java.util.List;
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
+    @Autowired
+    private UserService service;
 
     @GetMapping
     public ResponseEntity<List<User>> findAll() {
-        User maria = new User("1", "Maria Brown", "maria@gmail.com");
-        User alex = new User("2", "Alex Green", "alex@gmail.com");
-
-        List<User> list = new ArrayList<>();
-        list.addAll(Arrays.asList(maria, alex));
-
+        List<User> list = service.findAll();
         return ResponseEntity.ok().body(list);
     }
 }
---
+++ src/main/java/com/marcelomnzs/workshopmongo/services/UserService.java
@@ -0,0 +1,18 @@
+package com.marcelomnzs.workshopmongo.services;
+
+import com.marcelomnzs.workshopmongo.domain.User;
+import com.marcelomnzs.workshopmongo.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserService {
+    @Autowired
+    private UserRepository repository;
+
+    public List<User> findAll() {
+        return repository.findAll();
+    }
+}
