52045ce888f59b5c47a81f3290e4bcc2a9440263
Marcelo Antonio <marceloa.mnzs@gmail.com>
2025-10-16 17:32:39 -0300

    Obtendo um post por id

---
+++ src/main/java/com/marcelomnzs/workshopmongo/services/PostService.java
@@ -0,0 +1,19 @@
+package com.marcelomnzs.workshopmongo.services;
+
+import com.marcelomnzs.workshopmongo.domain.Post;
+import com.marcelomnzs.workshopmongo.repository.PostRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class PostService {
+    @Autowired
+    private PostRepository repository;
+
+    public Post findById(String id) {
+        Optional<Post> post = repository.findById(id);
+        return post.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+    }
+}
